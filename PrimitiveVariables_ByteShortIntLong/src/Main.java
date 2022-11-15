ab49323ba2886cf931ccd79c929f5b7503657709
Jeffrey Balbalosa <31784615+jbalbalosa@users.noreply.github.com>
2022-11-16 08:11:36 +0900

    Add to Java programming Masterclass Repo

--- PrimitiveVariables_ByteShortIntLong/src/Main.java
+++ PrimitiveVariables_ByteShortIntLong/src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args){
+        System.out.println("Primitive Variables Byte Short Int Long");
+        int myInt = 0;
+        byte myByte = 0;
+
+        System.out.println(myInt);
+        System.out.println(myByte);
+
     }
 }
